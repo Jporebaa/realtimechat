0e707bcbfaf63cbf330f5dfb2c1e7c094a6f6f15
Jakub Poręba <114797801+Jporebaa@users.noreply.github.com>
2023-11-15 11:38:45 +0100

    Add ComponentResizer (#18)
    Co-authored-by: Jporebaa <jakubporeba8@gmial.com>

--- part 23/chart-application/src/com/raven/main/Main.java
+++ part 23/chart-application/src/com/raven/main/Main.java
@@ -1,4 +1,27 @@
 
-public class Main {
+package com.raven.main;
+
+import com.raven.swing.ComponentResizer;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+
+
+public class Main extends javax.swing.JFrame {
+
+
+    public Main() {
+        init();
+    }
+
+    private void init() {
+        setIconImage(new ImageIcon(getClass().getResource("/com/raven/icon/icon.png")).getImage());
+        ComponentResizer com = new ComponentResizer();
+        com.registerComponent(this);
+        com.setMinimumSize(new Dimension(800, 500));
+        com.setMaximumSize(Toolkit.getDefaultToolkit().getScreenSize());
+        com.setSnapSize(new Dimension(10, 10));
+    }
 
 }
