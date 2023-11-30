6181a53785055ac3b51aac1f8452cfcf69ef7c15
dominik228820 <dominik228820@gmail.com>
2023-11-30 17:55:41 +0100

    #47 dodanie nazwy chatu oraz miejsca statusu aktywnosci

--- part 23/chart-application/src/com/raven/component/Chat_Title.java
+++ part 23/chart-application/src/com/raven/component/Chat_Title.java
@@ -1,11 +1,23 @@
 package com.raven.component;
 
-
+import java.awt.Color;
 public class Chat_Title extends javax.swing.JPanel {
 
 
     public Chat_Title() {
         initComponents();
+    }
+
+    public void setUserName(String userName) {
+        lbName.setText(userName);
+    }
+    public void statusActive(){
+        lbStatus.setText("Active now");
+        lbStatus.setForeground(new java.awt.Color(40,147,59));
+    }
+    public void setStatusText(String text){
+        lbStatus.setText(text);
+        lbStatus.setForeground(new Color(160,160,160));
     }
 
     @SuppressWarnings("unchecked")
@@ -26,5 +38,7 @@ public class Chat_Title extends javax.swing.JPanel {
     }// </editor-fold>//GEN-END:initComponents
 
 
-
+    private javax.swing.JLayeredPane layer;
+    private javax.swing.JLabel lbName;
+    private javax.swing.JLabel lbStatus;
 }
