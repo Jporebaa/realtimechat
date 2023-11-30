8fbf7565230fd161b2c55d48accb4516a864a455
Jporebaa <jakubporeba8@gmial.com>
2023-11-30 09:40:25 +0100

    Chat space

---
+++ part 23/chart-application/src/com/raven/component/Chat_Title.java
@@ -0,0 +1,30 @@
+package com.raven.component;
+
+
+public class Chat_Title extends javax.swing.JPanel {
+
+
+    public Chat_Title() {
+        initComponents();
+    }
+
+    @SuppressWarnings("unchecked")
+    private void initComponents() {
+
+        setBackground(new java.awt.Color(242, 242, 242));
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(this);
+        this.setLayout(layout);
+        layout.setHorizontalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+                layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                        .addGap(0, 40, Short.MAX_VALUE)
+        );
+    }// </editor-fold>//GEN-END:initComponents
+
+
+
+}
